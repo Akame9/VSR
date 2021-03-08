b7b054321513e36adac54c78a39557e6c7c08d07
brm9999 <bhavanamache99@gmail.com>
2021-03-08 14:46:31 +0530

    Updated mel-generation

--- B_generate_mels.py
+++ B_generate_mels.py
@@ -1,60 +1,53 @@
-#generating mel
 import B_audio
 from b_hparams import hparams as hp
 import numpy as np
 from os import listdir, path
+import os
 import librosa
 from scipy.io.wavfile import write
 
+def generating_mels(file,fulldir):
+    list=[0.0,0.4,0.8,1.2,1.6,2.0]
+    all_mels=[]
+    all_arrays=[]
+    for i in range(0,len(list)):
+        wavpath="C:/Project/VSR/B_dataset/wavs/"+str(file[29:35])+"/"+str(list[i])+"-1.wav"
+        wav = B_audio.load_wav(wavpath, hp["sample_rate"])
+        print(wav.shape)
+        print(type(wav))
+        spec = B_audio.melspectrogram(wav, hp)
+        #lspec = B_audio.linearspectrogram(wav, hp)
+        print("type",type(spec))
+        print("shape",spec.shape)
+        all_mels.append(np.transpose(spec))
+        print("shape of all mels",all_mels[i].shape)
+        if (i==5):
+            padded = np.zeros(all_mels[4].shape)
+            padded[:all_mels[5].shape[0]] = all_mels[5]
+            all_mels[5]=padded
+            print("shape after appending", all_mels[5].shape)
+        
 
-#ffmpeg to cut audio frames
-#from 0 sec go ahead 1 sec
-#ffmpeg -ss 0 -t 1 -i C:/Users/BHAVANA/Desktop/audio.wav C:/Users/BHAVANA/Desktop/frame0-0.1.wav
-#ffmpeg -ss 0.4 -t 1 -i C:/Users/BHAVANA/Desktop/audio.wav C:/Users/BHAVANA/Desktop/frame0-0.1.wav
-#ffmpeg -ss 0.8 -t 1 -i C:/Users/BHAVANA/Desktop/audio.wav C:/Users/BHAVANA/Desktop/frame0-0.1.wav
+        #reading all mels to create wavs for cross checking    
+        data= np.transpose(all_mels[i])
+        print(data.shape)
+        wav = B_audio.inv_mel_spectrogram(data,hp)
+        B_audio.save_wav(wav,'C:/Project/VSR/check_mels'+str(i)+'.wav' , sr=16000)
+        
+    #appending all files into 1 file
+    for i in range (0,len(all_mels)):
+        all_arrays.append(np.transpose(all_mels[i]))
+            
+    print("shape of all 6",np.shape(all_arrays))
+    np.save(fulldir+"all_6", all_arrays)
 
-#generating mel using B_audio functions
-wavpath="C:/Project/GRID_Data/frame1.wav"
-wav = B_audio.load_wav(wavpath, hp["sample_rate"])
-print(wav.shape)
-print(type(wav))
-fulldir="C:/Users/Bhavana/Desktop"
-specpath = path.join(fulldir, 'frame1.npz')
-spec = B_audio.melspectrogram(wav, hp)
-lspec = B_audio.linearspectrogram(wav, hp)
-np.savez_compressed(specpath, spec=spec, lspec=lspec)
-
-
-
-
-#Load the generated 6 mels with overlap of 15 frames
-data_1 = np.load('C:/Users/BHAVANA/Desktop/mels_0-1.npz')['spec'].T
-data_2 = np.load('C:/Users/BHAVANA/Desktop/mels_0.4-1.npz')['spec'].T
-data_3 = np.load('C:/Users/BHAVANA/Desktop/mels_0.8-1.npz')['spec'].T
-data_4 = np.load('C:/Users/BHAVANA/Desktop/mels_1.2-1.npz')['spec'].T
-data_5 = np.load('C:/Users/BHAVANA/Desktop/mels_1.6-1.npz')['spec'].T
-data_6 = np.load('C:/Users/BHAVANA/Desktop/mels_2.0-1.npz')['spec'].T
-
-'''print("1",data_1.shape)
-print("2",data_2.shape)
-print("3",data_3.shape)
-print("4",data_4.shape)
-print("5",data_5.shape)
-print("6",data_6.shape)'''
-
-#padding the last mel from (79,80) to (81,80)
-padded = np.zeros(data_5.shape)
-padded[:data_6.shape[0]] = data_6
-data_6=padded
-np.savez_compressed('C:/Users/BHAVANA/Desktop/mels_constructed.npz', spec=data_6) # save the padded data
-
-#load the padded data to convert to wav for cross-checking
-padded_data= np.load('C:/Users/BHAVANA/Desktop/mels_constructed.npz')['spec']
-wav = B_audio.inv_mel_spectrogram(padded_data.T,hp)
-B_audio.save_wav(wav,'C:/Project/VSR/code/check_mel.wav' , sr=16000)
+        
+def main():
+    file='C:/Project/VSR/B_dataset/s2/bbaf1n/audio.wav'
+    fulldir="C:/Project/VSR/B_dataset/all_in_one/"+str(file[29:35])
+    os.makedirs(fulldir, exist_ok=True)
+    generating_mels(file,fulldir)
 
 
-# Concatenate 
-np.savez_compressed('C:/Project/GRID_Data/set_1', a=data_1,b=data_2,c=data_3,d=data_4,e=data_5,f=data_6 )
-loaded = np.load('C:/Project/GRID_Data/set_1.npz')
-print(loaded.files)
+if __name__ == '__main__':
+	main()
